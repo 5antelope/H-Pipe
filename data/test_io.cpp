f53f1e053e8c2b32074cd6bb1bfd17eaf30e4a5d
wy4515 <y.wu4515@gmail.com>
2016-04-30 01:01:20 -0400

    able to read .pb file

--- data/test_io.cpp
+++ data/test_io.cpp
@@ -1,25 +1,29 @@
-#include "db.hpp"
 #include "data.pb.h"
 #include "io.hpp"
 #include <string>
+#include <fcntl.h>
+
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 
 int main(int argc, char* argv[]) {
 
     // Initialize Google's logging library.
     google::InitGoogleLogging(argv[0]);
 
-    string path="/home/yangwu/mnist/mnist_train_lmdb";
-
-    ifstream file;
-    file.open(path);
-
-    if (!file.is_open())
-        LOG(INFO) << "image file cannot be open";
+    string path="/home/yangwu/git/H-Net/data/inception_net.pb";
+    std::fstream input(path, ios::in | ios::binary);
 
     NetDef netDef;
-    netDef = netDef.ParseFromString(file);
-    
-    std::cout << "protbuf test pass";
+
+    if (!input) {
+        std::cout << path << ": File not found." << std::endl;
+    } else if (!netDef.ParseFromIstream(&input)) {
+        std::cerr << "Failed to parse address book." << std::endl;
+        return -1;
+    }
+
+    std::cout << "protbuf test pass" << std::endl;
 
     return 0;
 }
