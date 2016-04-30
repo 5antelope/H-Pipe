6d2cb9f634974d9c6bd17efe22c9ad83394cc9d1
wy4515 <y.wu4515@gmail.com>
2016-04-29 23:37:44 -0400

    working on input pb files

--- data/test_io.cpp
+++ data/test_io.cpp
@@ -1,40 +1,25 @@
 #include "db.hpp"
 #include "data.pb.h"
 #include "io.hpp"
-#include<string>
+#include <string>
+
 int main(int argc, char* argv[]) {
+
     // Initialize Google's logging library.
     google::InitGoogleLogging(argv[0]);
 
     string path="/home/yangwu/mnist/mnist_train_lmdb";
 
-    db::DB* cifar_db = db::GetDB("lmdb");
+    ifstream file;
+    file.open(path);
 
-    cifar_db->Open(path, db::READ);
-    db::Cursor* cur = cifar_db->NewCursor();
+    if (!file.is_open())
+        LOG(INFO) << "image file cannot be open";
 
-    int count = 0;
-    while(count < 5000) {
-        Datum datum;
-        datum.ParseFromString(cur->value());
+    NetDef netDef;
+    netDef = netDef.ParseFromString(file);
+    
+    std::cout << "protbuf test pass";
 
-        cv::Mat img;
-        img = DatumToCVMat(datum);
-
-        // Create window
-        /*cv::namedWindow( "Sample Image" , cv::WINDOW_NORMAL );
-        for(;;) {
-            int c;
-            c = cv::waitKey(10);
-            if( (char)c == 27 )
-            { break; }
-            imshow( "Sample Image", img);
-        }*/
-
-        // imwrite(std::to_string(count) + ".jpg", img);
-        count++;
-        cur->Next();
-    }
-    cifar_db->Close();
     return 0;
 }
